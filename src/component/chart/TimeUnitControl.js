c99d8e37f0107ecca7e9a8f6ad2d01c4abd35be8
jkh7562 <86720157+jkh7562@users.noreply.github.com>
2025-04-30 21:27:52 +0900

    수정

--- src/component/chart/TimeUnitControl.js
+++ src/component/chart/TimeUnitControl.js
@@ -5,15 +5,15 @@ const TimeUnitControl = ({ selectedUnit, onSelectUnit }) => {
     const units = ["연", "월", "일"];
 
     return (
-        <div className="border-white rounded-t-md bg-white flex gap-2 p-4">
+        <div className="border-white rounded-t-md bg-white flex gap-2 px-8 py-6">
             {units.map((unit) => (
                 <button
                     key={unit}
                     onClick={() => onSelectUnit(unit)}
-                    className={`text-xs border rounded px-2 py-1 ${
+                    className={`text-sm border rounded-md px-2 py-1 ${
                         selectedUnit === unit
-                            ? "bg-black text-white border-black"
-                            : "bg-white text-[#21262B] border-gray-300"
+                            ? "bg-[#21262B] text-white border-black"
+                            : "bg-white text-[#60697E] border-gray-300"
                     }`}
                 >
                     {unit}
